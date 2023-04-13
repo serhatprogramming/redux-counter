2a2603d37d5bcf196590c7cfb2678d521502d72a
serhatprogramming <serhatprogramming@gmail.com>
2023-04-13 12:02:15 -0400

    redux store creation

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { createStore } from "redux";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+      case "INCREMENT":
+        return state + 1;
+      case "DECREMENT":
+        return state - 1;
+      case "ZERO":
+        return 0;
+      default:
+        return state;
+    }
+  };
+
+  const store = createStore(counterReducer);
+  store.subscribe(() => {
+    const counterState = store.getState();
+    console.log("state of the counter: ", counterState);
+  });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "INCREMENT" });
+  store.dispatch({ type: "ZERO" });
+  store.dispatch({ type: "DECREMENT" });
+
+  return <div>Hello World</div>;
 }
 
 export default App;
